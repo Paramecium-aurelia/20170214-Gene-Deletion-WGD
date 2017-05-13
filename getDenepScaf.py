8c42d12d0e296d5d99c3b0e3c1d7b17efe78ad09
Enzabaru <enzabaru@149-161-250-212.dhcp-bl.indiana.edu>
2017-05-13 13:54:52 -0400

    Test, test!! losal

---
+++ getDenepScaf.py
@@ -0,0 +1,61 @@
+import re, sys, math, operator, copy, collections 
+
+def ProcessCLI(args):
+    scaf_pb=readgff(args[1])
+    #bestHits,onewayhits= readpsl(args[2],scaf_pb)
+    #print 'Best Hits : reciprocal hits'
+    #print '------------'
+    #for hit in bestHits:
+    #    print ' '.join(hit) 
+    #print '\n***********************\n'
+    #print 'One way hits'
+    #print '------------'
+    for hit1 in scaf_pb:
+        print hit1, len(scaf_pb[hit1])
+
+#Read in and update the psl output and find hits
+def readpsl(pslout,pbidict):
+    bhits=[]
+    hits=[]
+    with open(pslout,'r') as file:
+        for line in file:
+            line=line.rstrip().split()
+            ## Here are the filtering conditions: pident has to be than 50% and the minimum common subsequence overlap of  the querry and subject >=40%
+            ## These criteria could be tuned and changed as desired
+            if float(line[2])>50.0 and max([ int(i) for i in re.split(r'(\d+)',line[-1]) if len(i)>0 and i.isdigit()])/float(line[3])>=0.40:
+                if line[0]!=line[1]:
+                    maxover=max([ int(i) for i in re.split(r'(\d+)',line[-1]) if len(i)>0 and i.isdigit()])
+                    overlap=maxover/float(line[3])
+                    ### if the maximum coverage subsequence overlap % is 100%: this is the best hit
+                    if overlap>=1.0:
+                        bhits+=[(line[0],pbidict[line[0]],line[1],pbidict[line[1]],line[-2])]
+                    ### otherwise record the hit and then compare reciprocity later on
+                    else:
+                        hits+=[(line[0],pbidict[line[0]],line[1],pbidict[line[1]])]
+
+    ###Check reciprocal hits
+    onehits=copy.copy(hits)
+    for h in range(len(hits)-1):
+        for k in range(h,len(hits)):
+            if hits[h][0]==hits[k][1]: ## if reciprocal ie (1,2)=(2,1) then best hit and remove from onehits list 
+                bhits.append(hits[h])
+                onehits.remove(hits[h])
+
+    #     print line[0],pbidict[line[0]],line[1],pbidict[line[1]],' '.join(line[2:-1]),maxover, maxover/float(line[3])
+    return bhits,onehits
+#Read he gff3 files and create a dictionary: scaffolds: list of proteins                                   
+def readgff(f):
+    filename = open(f, "r")
+    scaffolds={}
+    pbi=collections.OrderedDict()
+    for line in filename:
+        line=line.strip().split() 
+        if line[0][:8]=='scaffold' and line[2]=='gene':
+            if line[0] not in pbi:
+                pbi[line[0]]=[re.split(r'[=;\s]',line[8])[1]]
+            else:
+                pbi[line[0]].append(re.split(r'[=;\s]',line[8])[1])
+    return pbi 
+
+if __name__ == '__main__':
+     ProcessCLI(sys.argv)
---
+++ withoutlogHitsOriginal.py
@@ -0,0 +1,69 @@
+from __future__ import division
+import re, sys, math, operator, copy,collections
+import numpy as np 
+
+def ProcessCLI(args):
+    #scaf_pb=readgff(args[1])
+    bestHits= readpsl(args[1])
+    for k in bestHits:
+        print ' \t'.join([str(i) for i in list(k)])
+
+#Read in and update the psl output and find hits
+def readpsl(pslout):
+    bhits=[]
+    hits=collections.OrderedDict()
+    phits=collections.OrderDict()
+    with open(pslout,'r') as file:
+        for line in file:
+            line=line.rstrip().split()
+            ## Here are the filtering conditions: pident has to be than 50% and the minimum common subsequence overlap of  the querry and subject >=40%
+            ## These criteria could be tuned and changed as desired
+            if float(line[4])>50.0 and max([ int(i) for i in re.split(r'(\d+)',line[-1]) if len(i)>0 and i.isdigit()])/min(float(line[5]),float(line[6]))>=0.40:
+                if line[0]!=line[2]:
+                    if line[1] not in hits.keys():
+                        hits[line[1]]=[line[3]]
+                    else:
+                        hits[line[1]].append(line[3])
+                
+#    print hits['scaffold_0001']
+#    return 
+
+    for key in hits:
+        #hits[key]=dict([a,x] for a, x in collections.Counter(hits[key]).iteritems())
+        hits[key]=dict([a,-math.log(float(x)/sum(collections.Counter(hits[key]).values()))] for a, x in collections.Counter(hits[key]).iteritems\
+())
+
+#    print hits['scaffold_0269']
+    for k1 in hits: 
+        for k2 in hits[k1]:
+            score1=0
+            score2=0
+            try:
+                score1=hits[k1][k2]
+                
+            except:
+                score1=10
+            #print k1,k2,score1
+            try:
+                score2=hits[k2][k1]
+            except:
+                score2=10
+            score=score1+score2
+            
+            bhits.append((k1,k2,score))
+                             
+    return sorted(bhits, key=lambda tup: (tup[2]) )#bhits
+
+#Read he gff3 files and create a dictionary: scaffolds: list of proteins                                   
+def readgff(f):
+    filename = open(f, "r")
+    scaffolds={}
+    pbi={}
+    for line in filename:
+        line=line.strip().split() 
+        if line[0][:8]=='scaffold' and line[2]=='mRNA':
+            pbi[re.split(r'[=;\s]',line[8])[1]]=line[0]
+    return pbi 
+
+if __name__ == '__main__':
+     ProcessCLI(sys.argv)
